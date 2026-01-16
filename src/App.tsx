feacba0221cc3284312ea99d22d7f07c6e1f24fc
Mathushalini00 <shalinimathu29@gmail.com>
2026-01-16 21:36:11 +0530

    change

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Layout from "../app/layout";
-import About from "../templates/about";
 import HomePage from "./templates/home";
+import Layout from "./app/layout";
+import About from "./templates/about";
 
 function ClientRouter() {
   return (
